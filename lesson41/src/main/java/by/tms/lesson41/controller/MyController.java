0ba35aff039f002eb7aca2fc494f67a193160b7c
Marina Yadevich <marishka.kazakova.2012@mail.ru>
2025-07-28 16:40:15 +0300

    Fix Logger after code review

--- lesson41/src/main/java/by/tms/lesson41/controller/MyController.java
+++ lesson41/src/main/java/by/tms/lesson41/controller/MyController.java
@@ -1,20 +1,18 @@
 package by.tms.lesson41.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class MyController {
 
     @Value("${app.bet.min:15}")
     private Integer min;
     @Value("${app.bet.max}")
     private Integer max;
-
-    private static final Logger log = LoggerFactory.getLogger(MyController.class);
 
     @GetMapping
     public String test(){
